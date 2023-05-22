065b12e6ac40ecb1b8a3a790cba49cb8c3f6e42c
JeonHyoChang <jhc1426@naver.com>
2023-05-23 01:19:58 +0900

    feat: 홈 API 통신기능 구현
    오픈, 클로즈 상태 기본 정보
    
    이슈: #62

---
+++ BE/src/main/java/team01/issuetracker/controller/pring/ResultVO.java
@@ -0,0 +1,13 @@
+package team01.issuetracker.controller.pring;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResultVO {
+    private int code;
+    private String message;
+}

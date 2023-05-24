8d377edc98fb6b7e3b6049ef1184e8e3b27bfc05
jaea-kim <jaeakim98@gmail.com>
2023-05-24 19:10:31 +0900

    remove: 필요없는 파일 삭제

--- BE/src/test/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into issue (title, description, is_open, member_id) values ('듀이입니다', '듀이는 테스트 중...', '0', '1');
-insert into issue (title, description, is_open, member_id, milestone_id) values ('만쥬입니다', '만쥬는 테스트 중...', '1', '2', '1');
-insert into issue (title, description, is_open, member_id) values ('코어입니다', '코어는 테스트 중...', '0', '2');
-insert into issue (title, description, is_open, member_id, milestone_id) values ('해나입니다', '해나는 테스트 중...', '1', '5', '2');
-insert into issue (title, description, is_open, member_id) values ('하림입니다', '하림는 테스트 중...', '0', '3');
-insert into issue (title, description, is_open, member_id, milestone_id) values ('다온입니다', '다온는 테스트 중...', '1', '4', '2');
-insert into issue (title, description, is_open, member_id, milestone_id) values ('포코입니다', '포코는 테스트 중...', '0', '1', '2');

4a2d41650e2a2215c39e4b1b3207e4a149432bd1
jaea-kim <jaeakim98@gmail.com>
2023-05-25 14:01:06 +0900

    refactor: 변수명 변경

--- BE/src/main/java/team01/issuetracker/service/dto/response/IssueResponseDTO.java
+++ BE/src/main/java/team01/issuetracker/service/dto/response/IssueResponseDTO.java
@@ -15,7 +15,7 @@ public class IssueResponseDTO {
 
     private final Long id;
     private final String title;
-    private final LocalDateTime createdDateTime; // 형식이 이상해서 잠시 스트링
+    private final LocalDateTime createAt; // 형식이 이상해서 잠시 스트링
     private final List<String> assignees; // 임시 리스트
     private final List<MiniLabel> labels; // 임시 리스트
     private final String milestone;
@@ -26,7 +26,7 @@ public class IssueResponseDTO {
         return IssueResponseDTO.builder()
                 .id(temp.id)
                 .title(temp.title)
-                .createdDateTime(temp.createdDateTime)
+                .createAt(temp.createAt)
                 .assignees(temp.assignees)
                 .labels(temp.labels)
                 .milestone(temp.milestone)
@@ -34,5 +34,4 @@ public class IssueResponseDTO {
                 .authorUrl(temp.authorUrl)
                 .build();
     }
-
 }

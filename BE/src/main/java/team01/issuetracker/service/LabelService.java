a0f87dd0512a06bb1007632b1d31edeb3c529e2a
jaea-kim <jaeakim98@gmail.com>
2023-05-25 14:02:13 +0900

    feat: 더미 데이터에서 db 데이터로 변경

--- BE/src/main/java/team01/issuetracker/service/LabelService.java
+++ BE/src/main/java/team01/issuetracker/service/LabelService.java
@@ -1,23 +1,25 @@
 package team01.issuetracker.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team01.issuetracker.domain.Label;
+import team01.issuetracker.repository.LabelRepository;
 import team01.issuetracker.service.dto.response.LabelDTO;
 import team01.issuetracker.service.dto.response.LabelsResponseDTO;
 import team01.issuetracker.service.vo.Count;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class LabelService {
+    @Autowired
+    LabelRepository labelRepository;
 
-    /*
-    이 아이는 듀이의 수정을 기다리고 있습니다 ㅠ
-     */
     public LabelsResponseDTO getLabels() {
+        //TODO: 카운터..는.. 나중에ㅜㅜ
         Count count = Count.builder() // 임시 값(명세서)
                 .label(4)
                 .milestone(2)
@@ -25,15 +27,9 @@ public class LabelService {
                 .closed(0)
                 .build();
 
-        List<LabelDTO> labels = new ArrayList<>();
-        labels.add(LabelDTO.of(new Label(1L, "FE", "FE 담당"
-                , "#655CC7", "#FFFFFF")));
-        labels.add(LabelDTO.of(new Label(2L, "BE", "BE 담당"
-                , "#EC8D8D", "#FFFFFF")));
-        labels.add(LabelDTO.of(new Label(3L, "iOS", "iOS 담당"
-                , "#A3D485", "#FFFFFF")));
-        labels.add(LabelDTO.of(new Label(4L, "bug", "버그 상황"
-                , "#14D6B6", "#FFFFFF")));
+        List<LabelDTO> labels = labelRepository.findAll().stream()
+                .map(l -> LabelDTO.of(l))
+                .collect(Collectors.toList());
 
         return LabelsResponseDTO.builder()
                 .count(count)

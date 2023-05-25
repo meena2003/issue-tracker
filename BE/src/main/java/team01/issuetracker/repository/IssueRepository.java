fc564fbe39a1e7cb4e103898ed380481146eacce
jaea-kim <jaeakim98@gmail.com>
2023-05-25 13:49:06 +0900

    feat: 상태로 이슈 찾기, 전체 찾기, 마일스톤id로 마일스톤 찾기 기능 추가

--- BE/src/main/java/team01/issuetracker/repository/IssueRepository.java
+++ BE/src/main/java/team01/issuetracker/repository/IssueRepository.java
@@ -1,8 +1,21 @@
 package team01.issuetracker.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import team01.issuetracker.domain.Issue;
 
+import java.util.List;
+
 public interface IssueRepository extends CrudRepository<Issue, Long> {
 
+    long countByIsOpen(boolean state);
+
+    List<Issue> findAllByIsOpen(boolean state);
+
+    List<Issue> findAll();
+
+    @Query("select * from issue where milestone_id = :id")
+    List<Issue> findByMilestone(Long id);
+
+    //TODO : 마일스톤 선택
 }

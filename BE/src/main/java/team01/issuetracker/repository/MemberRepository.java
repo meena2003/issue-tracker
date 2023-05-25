608b075370279862fe301a5b7163093bf59e8553
jaea-kim <jaeakim98@gmail.com>
2023-05-25 13:51:08 +0900

    feat: 멤버 전체 찾기 추가

--- BE/src/main/java/team01/issuetracker/repository/MemberRepository.java
+++ BE/src/main/java/team01/issuetracker/repository/MemberRepository.java
@@ -3,7 +3,8 @@ package team01.issuetracker.repository;
 import org.springframework.data.repository.CrudRepository;
 import team01.issuetracker.domain.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends CrudRepository<Member, Long> {
-
-
+    List<Member> findAll();
 }

5af76d3e1ee8027fc58be2ff7c825583e33c237b
jaea-kim <jaeakim98@gmail.com>
2023-05-30 22:45:27 +0900

    feat: 작성자 목록 조회 api 추가

--- BE/src/main/java/team01/issuetracker/controller/WriterController.java
+++ BE/src/main/java/team01/issuetracker/controller/WriterController.java
@@ -7,19 +7,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import team01.issuetracker.service.AssigneeService;
+import team01.issuetracker.service.WriterService;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/writers")
 @RestController
 public class WriterController {
 
-    private final AssigneeService assigneeService;
+    private final WriterService writerService;
     private final Logger logger = LoggerFactory.getLogger(WriterController.class);
 
     @GetMapping
-    public ResponseEntity<?> assigneesView() {
-        logger.info("작성자 조회");
-        return ResponseEntity.ok(assigneeService.getWriters());
+    public ResponseEntity<?> writersView() {
+        logger.info("작성자 목록 조회");
+        return ResponseEntity.ok(writerService.getWriters());
     }
 }

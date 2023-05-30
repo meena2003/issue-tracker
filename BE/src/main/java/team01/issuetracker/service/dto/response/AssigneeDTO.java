fc58794c04166ee31f0c32440c52b50f5d5343b7
Dewey <115435482+jaea-kim@users.noreply.github.com>
2023-05-31 00:05:39 +0900

    Revert "feat: 파마리터 타입 수정"
    This reverts commit 26aa24249396646048b9549e12c8ee98fe7956a8.

--- BE/src/main/java/team01/issuetracker/service/dto/response/AssigneeDTO.java
+++ BE/src/main/java/team01/issuetracker/service/dto/response/AssigneeDTO.java
@@ -3,7 +3,7 @@ package team01.issuetracker.service.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import team01.issuetracker.domain.Member;
+import team01.issuetracker.service.vo.Assignee;
 
 @Getter
 @Builder
@@ -14,11 +14,11 @@ public class AssigneeDTO {
     private final String name;
     private final String imgUrl;
 
-    public static AssigneeDTO of(Member assignee) {
+    public static AssigneeDTO of(Assignee assignee) {
         return AssigneeDTO.builder()
                 .id(assignee.getId())
                 .name(assignee.getName())
-                .imgUrl(assignee.getFileURL())
+                .imgUrl(assignee.getImgUrl())
                 .build();
     }
 }

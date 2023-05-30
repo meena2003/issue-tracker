83c84408a8ff4644f08a7037949d5e372311045a
Dewey <115435482+jaea-kim@users.noreply.github.com>
2023-05-31 00:05:39 +0900

    Revert "feat: Date 타입에서 LocalDate 로 변경"
    This reverts commit 16c78b8294c0aa969c32df549e3a0d62f18a2aca.

--- BE/src/main/java/team01/issuetracker/domain/Milestone.java
+++ BE/src/main/java/team01/issuetracker/domain/Milestone.java
@@ -6,9 +6,13 @@ import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
+import team01.issuetracker.service.dto.response.MilestoneDTO;
 
-import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Getter

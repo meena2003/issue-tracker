71552723ec74d9bcd41ec2ef251fe0a2cc8477f3
Dewey <115435482+jaea-kim@users.noreply.github.com>
2023-05-31 00:05:39 +0900

    Revert "feat: dto 생성 파라미터 변경"
    This reverts commit f610166bef2d2370d266d1822d751a178fd1168f.

--- BE/src/main/java/team01/issuetracker/service/dto/response/MilestoneDTO.java
+++ BE/src/main/java/team01/issuetracker/service/dto/response/MilestoneDTO.java
@@ -3,7 +3,7 @@ package team01.issuetracker.service.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import team01.issuetracker.domain.Milestone;
+import team01.issuetracker.service.vo.MilestoneVO;
 
 import java.time.LocalDate;
 
@@ -16,17 +16,17 @@ public class MilestoneDTO {
     private final String title;
     private final String description;
     private final LocalDate dueDate;
-    private final int openIssueCount;
-    private final int closedIssueCount;
+    private final Long openedIssue;
+    private final Long closedIssue;
 
-    public static MilestoneDTO of(Milestone milestone, int openIssueCount, int closedIssueCount) {
+    public static MilestoneDTO of(MilestoneVO milestoneVO) {
         return MilestoneDTO.builder()
-                .id(milestone.getId())
-                .title(milestone.getTitle())
-                .description(milestone.getDescription())
-                .dueDate(milestone.getDueDate())
-                .openIssueCount(openIssueCount)
-                .closedIssueCount(closedIssueCount)
+                .id(milestoneVO.getId())
+                .title(milestoneVO.getTitle())
+                .description(milestoneVO.getDescription())
+                .dueDate(milestoneVO.getDueDate())
+                .openedIssue(milestoneVO.getOpenedIssue())
+                .closedIssue(milestoneVO.getClosedIssue())
                 .build();
     }
 }

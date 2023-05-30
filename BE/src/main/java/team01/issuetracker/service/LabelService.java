a341abd0bf2a012de0c80a8f64a139143a83f617
jaea-kim <jaeakim98@gmail.com>
2023-05-30 22:41:44 +0900

    feat: 카운터 값 추가

--- BE/src/main/java/team01/issuetracker/service/LabelService.java
+++ BE/src/main/java/team01/issuetracker/service/LabelService.java
@@ -3,8 +3,8 @@ package team01.issuetracker.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import team01.issuetracker.domain.Label;
 import team01.issuetracker.repository.LabelRepository;
+import team01.issuetracker.repository.MilestoneRepository;
 import team01.issuetracker.service.dto.response.LabelDTO;
 import team01.issuetracker.service.dto.response.LabelsResponseDTO;
 import team01.issuetracker.service.vo.Count;
@@ -15,20 +15,21 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class LabelService {
+    //Todo: 고민거리: 서비스에서 모든 레포에 접근하게 하는게 맞나..?
     @Autowired
-    private final LabelRepository labelRepository;
+    LabelRepository labelRepository;
+    @Autowired
+    MilestoneRepository milestoneRepository;
+
 
     public LabelsResponseDTO getLabels() {
-        //TODO: 카운터..는.. 나중에ㅜㅜ
-        Count count = Count.builder() // 임시 값(명세서)
-                .label(4)
-                .milestone(2)
-                .opened(0)
-                .closed(0)
+        Count count = Count.builder()
+                .label((int) labelRepository.count())
+                .milestone(milestoneRepository.countByIsOpen(true))
                 .build();
 
         List<LabelDTO> labels = labelRepository.findAll().stream()
-                .map(l -> LabelDTO.of(l))
+                .map(LabelDTO::of)
                 .collect(Collectors.toList());
 
         return LabelsResponseDTO.builder()
@@ -36,31 +37,4 @@ public class LabelService {
                 .labels(labels)
                 .build();
     }
-
-    public void create(LabelDTO labelDTO) {
-        labelRepository.save(Label.create(labelDTO));
-    }
-
-    public void update(LabelDTO labelDTO, Long labelId) {
-        Label label = findLabelById(labelId);
-
-        label.update(labelDTO);
-
-        labelRepository.save(label);
-    }
-
-    public void delete(Long labelId) {
-        Label label = findLabelById(labelId);
-
-        label.getIssueLabels().stream()
-                .map(issueLabel -> issueLabel.deleteLabel(label));
-
-        labelRepository.delete(label);
-    }
-
-    public Label findLabelById(Long labelId) {
-        return labelRepository.findById(labelId)
-                .orElseThrow(() -> new RuntimeException("해당 레이블을 찾을 수 없습니다."));
-    }
-
 }

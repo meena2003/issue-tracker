b4df1d39ae362b98ccabd8f0c171685c9f85761f
jaea-kim <jaeakim98@gmail.com>
2023-05-31 00:38:14 +0900

    feat: 라벨 response 카운터 값 추가

--- BE/src/main/java/team01/issuetracker/service/LabelService.java
+++ BE/src/main/java/team01/issuetracker/service/LabelService.java
@@ -1,10 +1,10 @@
 package team01.issuetracker.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team01.issuetracker.domain.Label;
 import team01.issuetracker.repository.LabelRepository;
+import team01.issuetracker.repository.MilestoneRepository;
 import team01.issuetracker.service.dto.response.LabelDTO;
 import team01.issuetracker.service.dto.response.LabelsResponseDTO;
 import team01.issuetracker.service.vo.Count;
@@ -15,20 +15,17 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class LabelService {
-    @Autowired
     private final LabelRepository labelRepository;
+    private final MilestoneRepository milestoneRepository;
 
     public LabelsResponseDTO getLabels() {
-        //TODO: 카운터..는.. 나중에ㅜㅜ
-        Count count = Count.builder() // 임시 값(명세서)
-                .label(4)
-                .milestone(2)
-                .opened(0)
-                .closed(0)
+        Count count = Count.builder()
+                .label((int) labelRepository.count())
+                .milestone(milestoneRepository.countByIsOpen(true))
                 .build();
 
         List<LabelDTO> labels = labelRepository.findAll().stream()
-                .map(l -> LabelDTO.of(l))
+                .map(LabelDTO::of)
                 .collect(Collectors.toList());
 
         return LabelsResponseDTO.builder()

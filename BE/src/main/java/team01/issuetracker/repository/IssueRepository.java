d61e17e29d320d0e156b2b2ea14e555dd18be9f3
jaea-kim <jaeakim98@gmail.com>
2023-05-30 19:00:28 +0900

    feat: 필터 기능 query 문 추가

--- BE/src/main/java/team01/issuetracker/repository/IssueRepository.java
+++ BE/src/main/java/team01/issuetracker/repository/IssueRepository.java
@@ -2,6 +2,7 @@ package team01.issuetracker.repository;
 
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import team01.issuetracker.domain.Issue;
 
 import java.util.List;
@@ -15,7 +16,20 @@ public interface IssueRepository extends CrudRepository<Issue, Long> {
     List<Issue> findAll();
 
     @Query("select * from issue where milestone_id = :id")
-    List<Issue> findByMilestone(Long id);
+    List<Issue> findByMilestoneId(Long id);
 
-    //TODO : 마일스톤 선택
+    @Query("SELECT DISTINCT i.id, i.WRITER_ID, i.TITLE, i.DESCRIPTION, i.IS_OPEN, i.CREATE_DATE_TIME, i.MILESTONE_ID FROM issue i" +
+            "    LEFT OUTER JOIN issue_label il ON i.id = il.issue_id" +
+            "    LEFT OUTER JOIN assignee a ON i.id = a.issue_id" +
+            "    WHERE (:isOpen IS NULL OR i.is_open = :isOpen)" +
+            "    AND ((:milestone = 0 AND i.MILESTONE_ID IS NULL) OR (:milestone is NULL OR i.MILESTONE_ID = :milestone))" +
+            "    AND (:writers IS NULL OR i.writer_id = :writers)" +
+            "    AND ((:assignee IS NULL OR a.MEMBER_ID in (:assignee)) OR (a.MEMBER_ID IS NULL AND :assignee = 0))" +
+            "    AND ((:label IS NULL OR il.label_id in (:label)) OR (il.LABEL_ID IS NULL AND :label = 0))" +
+            "    ORDER BY i.id DESC;")
+    List<Issue> findAllByFilter(@Param("isOpen") boolean isOpen,
+                                @Param("milestone") Long milestone,
+                                @Param("label") Long label,
+                                @Param("assignee") Long assignee,
+                                @Param("writers") Long writers);
 }

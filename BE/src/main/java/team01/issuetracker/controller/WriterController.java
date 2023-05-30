1f5b62e569e998b0eaa2a0081ec61224ef292a64
jaea-kim <jaeakim98@gmail.com>
2023-05-31 00:47:52 +0900

    style: writer 이름 변경

--- BE/src/main/java/team01/issuetracker/controller/WriterController.java
+++ BE/src/main/java/team01/issuetracker/controller/WriterController.java
@@ -8,18 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team01.issuetracker.service.AssigneeService;
+import team01.issuetracker.service.WriterService;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/writers")
 @RestController
 public class WriterController {
 
-    private final AssigneeService assigneeService;
+    private final WriterService writerService;
     private final Logger logger = LoggerFactory.getLogger(WriterController.class);
 
     @GetMapping
-    public ResponseEntity<?> assigneesView() {
+    public ResponseEntity<?> writersView() {
         logger.info("작성자 조회");
-        return ResponseEntity.ok(assigneeService.getWriters());
+        return ResponseEntity.ok(writerService.getWriters());
     }
 }
--- BE/src/main/java/team01/issuetracker/service/dto/response/WritersResponseDTO.java
+++ BE/src/main/java/team01/issuetracker/service/dto/response/WritersResponseDTO.java
@@ -11,5 +11,5 @@ import java.util.List;
 @AllArgsConstructor
 public class WritersResponseDTO {
 
-    private final List<AssigneeDTO> writers;
+    private final List<MemberDTO> writers;
 }

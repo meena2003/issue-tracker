d0b0967aa261e4325010e1ffb1d1f090d5913337
JeonHyoChang <jhc1426@naver.com>
2023-05-30 22:43:27 +0900

    feat: 작성자 호출 기능 구현
    프론트의 요청입니다 ㅋ

---
+++ BE/src/main/java/team01/issuetracker/service/dto/response/WritersResponseDTO.java
@@ -0,0 +1,15 @@
+package team01.issuetracker.service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class WritersResponseDTO {
+
+    private final List<AssigneeDTO> writers;
+}

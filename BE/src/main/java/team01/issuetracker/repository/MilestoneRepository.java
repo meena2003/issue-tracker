547da3d1cf654eed7b3bcbedc60fd1315793fedd
jaea-kim <jaeakim98@gmail.com>
2023-05-31 19:21:17 +0900

    feat: 마일스톤 id로 마일스톤 찾기 쿼리 추가

--- BE/src/main/java/team01/issuetracker/repository/MilestoneRepository.java
+++ BE/src/main/java/team01/issuetracker/repository/MilestoneRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import team01.issuetracker.domain.Milestone;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MilestoneRepository extends CrudRepository<Milestone, Long> {
     List<Milestone> findAll();
@@ -11,4 +12,6 @@ public interface MilestoneRepository extends CrudRepository<Milestone, Long> {
     List<Milestone> findAllByIsOpen(boolean state);
 
     int countByIsOpen(boolean state);
+
+    Optional<Milestone> findById(Long id);
 }

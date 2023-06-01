28ea87a4acc552859badb303796c45b57ea6ac18
kimdongmin <meena2003@naver.com>
2023-06-01 12:15:07 +0900

    # Commit 컨벤션 # [Type]          [설명] # feat            새로운 기능 추가 # fix             버그 수정 # refactor        코드 리팩토링 (로직, 기능 수정) # design	        CSS 등 사용자 UI 디자인 추가 또는 변경 # style		        로직, 기능 변경 없는 오탈자, 띄어쓰기 수정 # comment         주석 추가 및 수정 # docs            README와 같은 문서 수정 # rename          파일, 폴더명 수정 혹은 이동 # remove	        파일, 폴더 삭제 # add		          파일 추가 # test		        테스트 코드 추가 또는 수정 # chore		        빌드 시스템, 패키지 관리자 설정 등의 변경 # revert	        이전 커밋으로 되돌아감 # !HOTFIX	        치명적인 버그를 고쳐야하는 경우 # # "[type]: 헤더" 형태로 작성 # 헤더는 커밋 내역에 대한 요약을 간단히 작성 # -------헤더------- "[add]: S3 관련 파일 3개 추가" # ----------------- ### 바로 아래 공백은 지우지 마]세요 (헤더와 내용의 분리를 위함)
    # 내용은 어떻게 변경했는지 보단 무엇을 변경했고 왜 변경했는지에 대한 설명 작성
    # -------내용-------
    깃 이력이 꼬여서 새롭게 브랜치를 만들고 S3 파일 3개를 생성한 후 커밋
    # -----------------
    ### 바로 아래 공백은 지우지 마세요 (내용과 꼬릿말의 분리를 위함)
    
    # [Keyword]	  [설명]
    # related to 	진행 중인 이슈
    # close		    이슈 해결했을 때 사용
    # ref		      참고할 이슈가 있을 때 사용
    #
    # "[Keyword]: #이슈번호, [Keyword]: #이슈번호 #이슈번호" 형태로 작성
    # 이슈를 추적하기 위한 용도로 사용
    # -------꼬릿말------
    
    # ----------------

---
+++ BE/src/main/java/team01/issuetracker/aws/FileUploadController.java
@@ -0,0 +1,21 @@
+package team01.issuetracker.aws;
+
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+@RestController
+public class FileUploadController {
+    private final FileUploadService fileUploadService;
+
+    public FileUploadController(FileUploadService fileUploadService) {
+        this.fileUploadService = fileUploadService;
+    }
+
+    @PostMapping("/upload")
+    public PutObjectResponse uploadFile(@RequestParam(value="file") MultipartFile file) {
+        return fileUploadService.uploadFile(file);
+    }
+}
---
+++ BE/src/main/java/team01/issuetracker/aws/FileUploadService.java
@@ -0,0 +1,44 @@
+package team01.issuetracker.aws;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class FileUploadService {
+
+    @Getter
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public PutObjectResponse uploadFile(MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        String contentType = file.getContentType();
+
+        // Upload file
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key("profile/" + fileName)
+                .acl("public-read")
+                .contentDisposition("inline")
+                .contentType(contentType)
+                .build();
+
+        try {
+            return s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to upload file", e);
+        }
+    }
+}
